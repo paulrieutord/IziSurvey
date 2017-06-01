5c37345873f8ce63450c88cc0081e2003be592be
Paul Rieutord <paulr812@gmail.com>
2017-06-01 01:43:37 -0400

    fix fragment in tab

--- app/src/main/java/com/udp/paul/izisurvey/ui/fm_surveys.java
+++ app/src/main/java/com/udp/paul/izisurvey/ui/fm_surveys.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,7 +32,7 @@ public class fm_surveys extends Fragment {
         return rootView;
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
